b8905d606e672f70171678281c30c824f34efc55
Jennifer Page <jenipage1989+github@gmail.com>
2025-01-30 07:58:23 -0500

    fix(alacritty): gremlin plugin -> gremlin feature

--- features/alacritty.sh
+++ features/alacritty.sh
@@ -130,8 +130,8 @@ install_linux() {
     # Assumption: there is only one directory in the tarball
     cd "$tempdir/$(ls "$tempdir")"
 
-    gremlin plugin asdf execute asdf local rust latest
-    gremlin plugin asdf execute cargo build --release
+    gremlin feature asdf execute asdf local rust latest
+    gremlin feature asdf execute cargo build --release
 
     as_root cp ./target/release/alacritty /usr/local/bin/
     as_root chown root:root /usr/local/bin/alacritty
